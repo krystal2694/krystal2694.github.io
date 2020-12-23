e3d0dad9e00280bb1f813ae08537cc07f34a36d2
krystal2694 <pwong2632my.bcit.ca>
2020-12-22 17:32:28 -0800

    completed base for credentials page

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,9 +5,9 @@ import "../css/Card.scss"
 function Card(props) {
 
     return(
-        <div className="card-container">
+        <div className="card-container" style={{width: `${props.width}`}}>
             <div className="header">
-                <h2 className="title"><span className='title-dots'> - </span>{props.title} <span className='title-dots'> - </span></h2>
+                <h2 className="title"><span className='title-dots'>  </span>{props.title} <span className='title-dots'>  </span></h2>
             </div>
                 <div className="content">{props.content}</div>
         </div>
--- src/views/Credentials.js
+++ src/views/Credentials.js
@@ -7,27 +7,41 @@ class Credentials extends Component {
     render() {
         const skillsContent = <div>
             <h4>Languages</h4>
-            <h5>English . Cantonese . Mandarin</h5>
-            <h4>Languages</h4>
-            <h5>English . Cantonese . Mandarin</h5>
+            <h5>English • Cantonese • Mandarin</h5>
+            <h4>Programming</h4>
+            <h5>Python • Java • C</h5>
+            <h4>Web</h4>
+            <h5>HTML • CSS/SCSS • JavaScript • React</h5>
+            <h4>More</h4>
+            <h5>SQL • Firebase • Agile • Git</h5>
         </div>;
 
         const experienceContent = <div>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
+            <h4>The Boeing Company</h4>
+            <h5>Software Quality Assurance Intern</h5>
+            <p>01.2020 - 08.2020</p>
+            <h4>The Keg Steakhouse and Bar</h4>
+            <h5>Trainer, Member of Opening Team, Cash Manager, Server, Guest Services</h5>
+            <p>04.2013 - 08.2020</p>
+        </div>;
+
+        const educationContent = <div>
+            <h4>British Columbia Institute of Technology</h4>
+            <h5>Computer Systems Technology - GPA: 95%</h5>
+            <p>01.2019 - Current</p>
+            <h4>University of British Columbia</h4>
+            <h5>Psychology and Sociology courses</h5>
+            <p>09.2012 - 04.2014</p>
         </div>;
         return (
             <div className="view">
                 <div className="credentials-container">
-                    <Card title="Skills" content={skillsContent}/>
-                    <Card title="Work Experience" content={experienceContent}/>
-                    <Card title="Education" content={skillsContent}/>
+                    <Card width={'25em'} title="Skills" content={skillsContent}/>
+                    <Card width={'25em'} title="Education" content={educationContent}/>
+                    <Card width={'56em'} title="Work Experience" content={experienceContent}/>
 
-                {/*<div className="content">*/}
+
+                    {/*<div className="content">*/}
                 {/*    <div className="content--inner">*/}
                 {/*    </div>*/}
                 {/*</div>*/}
